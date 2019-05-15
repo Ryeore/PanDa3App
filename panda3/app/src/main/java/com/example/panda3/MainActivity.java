e2bf05b990ce3793c7199fdfac00845c274b617e
Ryeore <kamil19970@gmail.com>
2019-05-15 18:22:11 +0200

    project restore

---
+++ panda3/app/src/main/java/com/example/panda3/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.panda3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_notes:
+                startActivity(new Intent(MainActivity.this, NotesActivity.class));
+                return true;
+
+            case R.id.action_calendar:
+                startActivity(new Intent(MainActivity.this, InfoActivity.class));
+                return true;
+
+            case R.id.action_tasks:
+                startActivity(new Intent(MainActivity.this, TasksActivity.class));
+                return true;
+
+            case R.id.action_about:
+                startActivity(new Intent(MainActivity.this, AboutActivity.class));
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+
+}
