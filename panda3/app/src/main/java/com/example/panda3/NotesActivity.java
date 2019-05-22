cebfb1563a8e138f066eaac6ab08a01285a9c73f
Michał <michalo238296@gmail.com>
2019-05-22 17:05:14 +0200

    Update NotesActivity.java

--- panda3/app/src/main/java/com/example/panda3/NotesActivity.java
+++ panda3/app/src/main/java/com/example/panda3/NotesActivity.java
@@ -11,10 +11,16 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.ListView;
 
+import java.util.HashMap;
+import java.util.Map;
 import android.database.Cursor;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.Toast;
+
+import android.widget.SimpleAdapter;
+import android.util.Log;
+import android.widget.AdapterView;
 
 import java.util.ArrayList;
 
@@ -32,19 +38,30 @@ public class NotesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
 
-        ListView listView = (ListView) findViewById(R.id.listView);
+//        ListView mlistView = (ListView) findViewById(R.id.listView);
         myDB = new DatabaseHelper(this);
 
-        ArrayList<String> theList = new ArrayList<>();
+//        ArrayList<String> theList = new ArrayList<>();
+//        ArrayList<String> theList1 = new ArrayList<>();
         Cursor data = myDB.getData();
+        ArrayList<Map<String,Object>> itemDataList = new ArrayList<Map<String,Object>>();;
         if(data.getCount() == 0){
             Toast.makeText(this, "There are no contents in this list!",Toast.LENGTH_LONG).show();
         }else{
             while(data.moveToNext()){
-                theList.add(data.getString(1));
-                ListAdapter listAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,theList);
-                listView.setAdapter(listAdapter);
+//                theList.add(data.getString(1));
+//                theList1.add(data.getString(3) + ":" + data.getString(4));
+                Map<String,Object> listItemMap = new HashMap<String,Object>();
+                listItemMap.put("name", data.getString(1));
+                listItemMap.put("time", data.getString(3) + ":" + data.getString(4));
+                itemDataList.add(listItemMap);
+//                ListAdapter listAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,theList);
+//                mlistView.setAdapter(listAdapter);
             }
+            SimpleAdapter simpleAdapter = new SimpleAdapter(this,itemDataList,android.R.layout.simple_list_item_2,
+                    new String[]{"title","description"},new int[]{android.R.id.text1,android.R.id.text2});
+            ListView listView = (ListView)findViewById(R.id.listView);
+            listView.setAdapter(simpleAdapter);
         }
 
 
@@ -77,6 +94,34 @@ public class NotesActivity extends AppCompatActivity {
                                             }
                                         }
         );
+
+//        mlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                String name = adapterView.getItemAtPosition(i).toString();
+//                Log.d(TAG, "onItemClick: You Clicked on " + name);
+//
+//                Cursor data = myDB.getItemID(name); //get the id associated with that name
+//                int itemID = -1;
+//                while(data.moveToNext()){
+//                    itemID = data.getInt(0);
+//                }
+//                if(itemID > -1){
+//                    Log.d(TAG, "onItemClick: The ID is: " + itemID);
+//                    Intent editScreenIntent = new Intent(NotesActivity.this, EditTaskActivity.class);
+//                    editScreenIntent.putExtra("id",itemID);
+//                    editScreenIntent.putExtra("name",name);
+//                    editScreenIntent.putExtra("date",date);
+////                    editScreenIntent.putExtra("hour",hour);
+////                    editScreenIntent.putExtra("minute",minute);
+//                    startActivity(editScreenIntent);
+//                }
+//                else{
+//                    toastMessage("No ID associated with that name: " + name);
+//                }
+//            }
+//        });
+
 
     }
 
@@ -119,4 +164,7 @@ public class NotesActivity extends AppCompatActivity {
             }
         });
     }*/
+    private void toastMessage(String message){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+    }
 }
