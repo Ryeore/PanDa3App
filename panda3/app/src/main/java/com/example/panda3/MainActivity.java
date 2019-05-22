46be1baed12c8bc466e717bdaca6daf72c09f0c7
xMaZzzI <48214457+xMaZzzI@users.noreply.github.com>
2019-05-22 20:40:52 +0200

    Add files via upload

--- panda3/app/src/main/java/com/example/panda3/MainActivity.java
+++ panda3/app/src/main/java/com/example/panda3/MainActivity.java
@@ -8,19 +8,36 @@ import android.widget.ImageButton;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+    private ProgressBar progressBar;
+    private TextView textView;
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = (TextView) findViewById(R.id.textView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
+        progressAnimation();
 
+    }
+
+    public void progressAnimation(){
+        ProgressBarAnimation anim = new ProgressBarAnimation(this,progressBar, textView, 0f, 100f);
+        anim.setDuration(SPLASH_TIME_OUT);
+        progressBar.setAnimation(anim);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
